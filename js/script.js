7228b2e05829b58ca52018ea849fbfcfed11d5c3
Amie Jensen <amie33@juno.com>
2021-01-24 17:26:46 -0700

    fade effect working..need to adjust some html elements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
+$(document).ready(function(){
+    $("#preloader").fadeOut(15000);
+    $("#wrapperAll").fadeIn(11000);
+})
 
+window.onload = function() {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    //window.setTimeout(showPage, 9000); 
+    
+};
+/*
+function showPage(){
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('wrapperAll').style.display = 'block';
+}
+*/
 
