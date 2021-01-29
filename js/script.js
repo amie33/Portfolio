46a6b95f91c81f77a0c6746c11feb644a592a7a5
Amie Jensen <amie33@juno.com>
2021-01-29 15:10:48 -0700

    added the moving background

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
+/**
+ * function to create the preloader page
+ */
 $(document).ready(function(){
-    // $("#preloader").fadeToggle(15000, function(){
-    //     //$("#wrapperAll").css("display", "block");
-    //     $("#wrapperAll").fadeIn(1000);  
-    // });
-    $("#preloader").fadeOut(9999, function(){
-        $("#wrapperAll").fadeIn(500); 
+    $("#preloader").fadeOut(9000, function(){
+        $("#wrapperAll").fadeIn(200); 
     })
     
 })
 
+/**
+ * so the page always starts at the top when loaded
+ */
 window.onload = function() {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
     //window.setTimeout(showPage, 9000); 
-    
 };
-/*
-function showPage(){
-    document.getElementById('preloader').style.display = 'none';
-    document.getElementById('wrapperAll').style.display = 'block';
-}
-*/
+
+
 
