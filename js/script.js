71e3d4bc997afe740a137cd31ce509d1ae5ae44d
Amie Jensen <amie33@juno.com>
2021-01-31 21:45:39 -0700

    fixed the transisiton preloader only shows once..now the fix the nav bar disappearing

--- js/script.js
+++ js/script.js
@@ -2,20 +2,31 @@
 /**
  * function to create the preloader page
  */
-$(document).ready(function(){
-    $("#preloader").fadeOut(10, function(){
-        $("#wrapperAll").fadeIn(500); 
-    })
+// $(document).ready(function(){
+//     $("#preloader").fadeOut(10, function(){
+//         $("#wrapperAll").fadeIn(500); 
+//     })
+// })
+jQuery(window).on('load', function(){
+    jQuery('#wrapperAll').hide();
+    if (sessionStorage.getItem('dontLoad') == null)
+    {
+        jQuery("#preloader").fadeOut(10000, function(){
+            $('#wrapperAll').fadeIn(500);
+            
+        });
+    }
+        
     
 })
 
 /**
  * so the page always starts at the top when loaded
  */
-window.onload = function() {
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-    //window.setTimeout(showPage, 9000); 
-};
+// window.onload = function() {
+//     document.body.scrollTop = document.documentElement.scrollTop = 0;
+//     //window.setTimeout(showPage, 9000); 
+// };
 
 
 
